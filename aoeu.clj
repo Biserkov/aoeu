6a9830e0da6c00d1bf9fbf79bcd73eda06b28c46
Jordan Biserkov <jordan@biserkov.com>
2015-10-29 10:04:08 +0200

    Use global bindings for better line number in exception.

--- aoeu.clj
+++ aoeu.clj
@@ -1,8 +1,9 @@
 (ns aoeu
   (:require [clojure.data.int-map :as s]))
 
-(let [a [1 139]
-      set0 (s/int-set a)
-      set1 (s/int-set [0])
-      set2 (s/difference set0 (s/int-set a))]
-  (s/union set1 set2))
+(def a [1 139])
+(def set0 (s/int-set a))
+(def set1 (s/int-set [0]))
+(def set2 (s/difference set0 (s/int-set a)))
+  
+(s/union set1 set2)
